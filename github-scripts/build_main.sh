d52beb6e29ee6575399b20b05483ffbd6a13e23e
Ben Willmore <ben@willmore.eu>
2025-04-05 13:10:41 +0100

    Updated build process

--- github-scripts/build_main.sh
+++ github-scripts/build_main.sh
@@ -5,10 +5,10 @@ DOCKERROOT=/root
 BUILDSCRIPT=build.sh
 
 NAME=gl4es
-cd ./build-scripts/
+cd ./github-scripts/
 ./docker-setup.txt $NAME-build
 cd ..
 
 sleep 5
 
-docker exec $NAME-build /bin/bash -c "./build-scripts/build.sh"
+docker exec $NAME-build /bin/bash -c "./github-scripts/build.sh"
